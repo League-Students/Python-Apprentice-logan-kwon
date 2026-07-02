6c7e72d5b2067336ac9335764cee8f2b12c9b445
League Student <student@jointheleague.org>
2026-07-02 13:25:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/40_Loops/30_Loop_with_Turtle.py
+++ lessons/10_Turtles/40_Loops/30_Loop_with_Turtle.py
@@ -1,18 +1,8 @@
-"""
-# 30_Loop_with_Turtle.py
+import turtle
+tina = turtle.Turtle()
+tina.goto(0,0)
+tina.pen
 
-In this program, use a loop to draw a regular pentagon (5-sided shape) with Tina the Turtle.
-
-- Review your previous program, 20_Loop_with_Turtle.py, which uses a loop to draw a shape with the turtle module.
-- Make sure your code is clear and well-commented.
-- Run your program to verify that Tina the Turtle draws a pentagon.
-
-(Hint: You can copy and modify your previous code!)
-
-uid: BpGnQq64
-name: Loop With Turtle
-"""
-
-... # Your code here
 for i in range(5)
-    tina.forwar
+    tina.left(72)
+    tina.forward(150)
