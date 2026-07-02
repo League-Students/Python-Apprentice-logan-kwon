acb5d29f8e0e089251205f3112edaa0a9979f449
League Student <student@jointheleague.org>
2026-07-02 12:51:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/30_Turtle_Tricks/20_Turtle_Tricks.py
+++ lessons/10_Turtles/30_Turtle_Tricks/20_Turtle_Tricks.py
@@ -1,17 +1,18 @@
-
 import turtle                           # Tell Python we want to work with the turtle
 turtle.setup(600, 600, 0, 0)            # Set the size of the window
 tina = turtle.Turtle()                  # Create a turtle named tina
 
 ... # Your code here
-def make_side(x=0,y=0):
     tina.left(36)
     tina.forward(50)
-make_side
+tina.left(36)
+tina.forward(50)
 tina.pencolor("blue")
-make_side
+tina.left(36)
+tina.forward(50)
 tina.pencolor("green")
-make_side
+tina.left(36)
+tina.forward(50)
 tina.pencolor("purple")
 make_side
 tina.pencolor("yellow")
