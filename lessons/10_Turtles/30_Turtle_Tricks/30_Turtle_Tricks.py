5ac5e3f6e4d6733c43c8afc30def417b4375e3a1
League Student <student@jointheleague.org>
2026-07-02 13:10:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/30_Turtle_Tricks/30_Turtle_Tricks.py
+++ lessons/10_Turtles/30_Turtle_Tricks/30_Turtle_Tricks.py
@@ -22,7 +22,10 @@ tina.circle(radius, steps=30)
 tina.end_fill()
 tina.penup()
 tina.goto(50,50)
-
+tina.pendown()
+tina.begin_fill()
+tina.circle(radius,steps=20)
+tina.end_fill()
 # Use tina.circle() to draw a circle, and tina.goto() to move tina to a new location
 # Use tina.begin_fill(), tina.end_fill(), and tina.fillcolor() to fill in the shapes
 
