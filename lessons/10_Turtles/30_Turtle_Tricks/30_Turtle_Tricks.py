524e820b3630f1e3b6ef797cebe96bf97da6db34
League Student <student@jointheleague.org>
2026-07-02 13:11:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/30_Turtle_Tricks/30_Turtle_Tricks.py
+++ lessons/10_Turtles/30_Turtle_Tricks/30_Turtle_Tricks.py
@@ -1,18 +1,4 @@
-"""
-# 30_Turtle_Tricks.py
-
-In this assignment, you will use Tina the Turtle to draw multiple shapes on the screen.
-
-- Draw two circles, each filled with a different color.
-- Position the circles in different locations on the screen (they should not overlap).
-- Use the turtle commands: begin_fill(), end_fill(), fillcolor(), circle(), and goto() to complete the task.
-- Try different colors and positions.
-
-Refer to the previous program, Meet_Tina.py for examples of how to use these turtle commands.
-"""
-
-# These lines are needed in most turtle programs
-import turtle                           # Tell Python we want to work with the turtle
+import turtle
 turtle.setup(600, 600, 0, 0)            # Set the size of the window
 tina = turtle.Turtle() 
 tina.goto(0,0)
