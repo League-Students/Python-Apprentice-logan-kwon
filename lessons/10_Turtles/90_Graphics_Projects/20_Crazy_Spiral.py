b8bd1062fda53d732618323259606b27689b9d6e
League Student <student@jointheleague.org>
2026-07-07 13:28:11 -0700

    Automated commit

--- lessons/10_Turtles/90_Graphics_Projects/20_Crazy_Spiral.py
+++ lessons/10_Turtles/90_Graphics_Projects/20_Crazy_Spiral.py
@@ -19,7 +19,7 @@ t = turtle.Turtle()... # Create a turtle named t
 def make_a_shape(t):
     """Make a shape with turtle t. Make it go left or right or forward"""    
     t.forward(30)
-
+    t,left(60)
 # 2) Call make_a_shape() in a loop to make the turtle draw a spiral.
 # For instance, you can call make_a_shape() 100 times to make a spiral with 100 shapes.
 # The second ... in the for loop should be the number of shapes you want to make,
