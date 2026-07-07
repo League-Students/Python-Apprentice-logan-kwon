676444dd0919ab9db120595783e3bfdb0b18ff3c
League Student <student@jointheleague.org>
2026-07-07 12:41:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/70_Projects/10_LeagueBot.py
+++ lessons/10_Turtles/70_Projects/10_LeagueBot.py
@@ -15,7 +15,7 @@ def set_turtle_image(turtle, image_name):
     """Set the turtle's shape to a custom image."""
 
     from pathlib import Path                        # Import Path from pathlib module
-    image_dir = Path(__file__).parent / "images"    # Define the directory containing images
+    image_dir = Path(__file__).parent.parent / "images"    # Define the directory containing images
     image_path = str(image_dir / image_name)        # Create the full path to the image file
 
     screen = turtle.getscreen()                     # Get the turtle's screen
@@ -29,7 +29,7 @@ screen.setup(width=600, height=600)
 # Create a turtle and set its shape to the custom GIF
 t = turtle.Turtle()
 
-set_turtle_image(t, "pikachu.gif")
+set_turtle_image(t, "leaguebot_bot.gif")
 
 t.penup()   # Prevent drawing when moving
 t.speed(3)  # Set a moderate speed
