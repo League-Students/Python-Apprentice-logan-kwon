1568474aea965fe63dbcf43a84ee032da71c5d92
League Student <student@jointheleague.org>
2026-07-07 13:16:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/70_Projects/30_Tash_Me_Click.py
+++ lessons/10_Turtles/70_Projects/30_Tash_Me_Click.py
@@ -32,32 +32,14 @@ def set_turtle_image(turtle, image_name):
     screen.addshape(image_path)                     # Register the image as a shape
     turtle.shape(image_path)                        # Set the turtle's shape to the image
 
-# Set up the screen
-screen = turtle.Screen()
-screen.setup(width=600, height=600)
-# Create a turtle and set its shape to the custom GIF
-t = turtle.Turtle()
+def screen_clicked(x, y):
+    """Print the x and y coordinates of the screen when clicked.
+    and make the turtle move to the clicked location."""
 
-set_turtle_image(t, 'moustache1.gif')   # Make the turtle really big
+    print('You pressed: x=' + str(x) + ', y=' + str(y))
 
-def turtle_clicked(t, x, y):
-    """Function that gets called when the user clicks on the turtle
-
-    This function will make the turtle tilt 20 degrees 18 times, making a full
-    circle. It is called by the turtle when the user clicks on it.
-
-    Args:
-        t (Turtle): The turtle object that was clicked
-        x (int): The x coordinate of the click
-        y (int): The y coordinate of the click
-    """
-
-    print('turtle clicked!')
-    
-    for i in range(0,360, 20):  # Full circle, 20 degrees at a time
-        t.tilt(20)              # Tilt the turtle 20 degrees
-
-# Connect the turtle to the turtle_clicked function
-t.onclick(lambda x, y, t=t: turtle_clicked(t, x, y))
+    t.goto(x, y) # Move the turtle to the clicked location
+  
+screen.onclick(screen_clicked) # Important! Tell Python which function to use when the screen is clicked
 
 turtle.done()
