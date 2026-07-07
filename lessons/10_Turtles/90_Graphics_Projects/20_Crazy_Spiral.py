7953c17c7e47504705d851463b769c95e1cf2266
League Student <student@jointheleague.org>
2026-07-07 13:28:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/90_Graphics_Projects/20_Crazy_Spiral.py
+++ lessons/10_Turtles/90_Graphics_Projects/20_Crazy_Spiral.py
@@ -10,7 +10,7 @@ name: Crazy Spiral
 
 ... # Copy code to make a turtle and set up the window
 
-t = ... # Create a turtle named t
+t = turtle.Turtle()... # Create a turtle named t
 
 # 1) Complete make_a_shape() to make the turtle move in some pattern. 
 # For instance, you can make it go left 30 degrees, then forward 50 pixels, 
@@ -18,7 +18,7 @@ t = ... # Create a turtle named t
 
 def make_a_shape(t):
     """Make a shape with turtle t. Make it go left or right or forward"""    
-    ...
+    t.forward(30)
 
 # 2) Call make_a_shape() in a loop to make the turtle draw a spiral.
 # For instance, you can call make_a_shape() 100 times to make a spiral with 100 shapes.
