41fa95ea0e30a9b92a9189dab7c8532b32b22c3d
League Student <student@jointheleague.org>
2026-07-10 12:42:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/10_Welcome/FNAF.py
+++ lessons/10_Turtles/10_Welcome/FNAF.py
@@ -5,10 +5,11 @@ tina = turtle.Turtle()
 screen = turtle.Screen()
 screen.setup(500,500)
 
-cam_colors = ["red", "black", "green", "white", "blue"]
+cam_colors = ["white", "blue", "red", "black", "blue"]
 
 def open_cam_1():
     print("CAM 1 OPENED")
+    screen.bgcolor(cam_colors[0])
 
 
 
