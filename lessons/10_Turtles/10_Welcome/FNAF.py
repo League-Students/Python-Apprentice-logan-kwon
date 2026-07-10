1cce83c02e6c45e7227c52fa3e58cdf14a1bdb85
League Student <student@jointheleague.org>
2026-07-10 13:05:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/10_Welcome/FNAF.py
+++ lessons/10_Turtles/10_Welcome/FNAF.py
@@ -8,7 +8,7 @@ tina_progress = 0
 screen = turtle.Screen()
 screen.setup(600,600)
 
-cam_colors = ["white", "blue", "red", "black", "green"]
+cam_colors = ["white", "blue", "red", "gray", "green"]
 
 def show_animatronics(cam_num):
     if(cam_num == tina_path[tina_progress]):
@@ -20,18 +20,23 @@ def show_animatronics(cam_num):
 def open_cam_1():
     print("CAM 1 OPENED")
     screen.bgcolor(cam_colors[0])
+    show_animatronics(1)
 def open_cam_2():
     print("CAM 2 OPENED")
     screen.bgcolor(cam_colors[1])
+    show_animatronics(2)
 def open_cam_3():
     print("CAM 3 OPENED")
     screen.bgcolor(cam_colors[2])
+    show_animatronics(3)
 def open_cam_4():
     print("CAM 4 OPENED")
     screen.bgcolor(cam_colors[3])
+    show_animatronics(4)
 def open_cam_5():
     print("CAM 5 OPENED")
     screen.bgcolor(cam_colors[4])
+    show_animatronics(5)
 def exit_cam():
     print("CAM EXITED")
     screen.bgcolor("yellow")
