a3262a98ae3a55de551e874640a1e3f0e841fe8d
League Student <student@jointheleague.org>
2026-07-10 13:01:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/10_Welcome/FNAF.py
+++ lessons/10_Turtles/10_Welcome/FNAF.py
@@ -11,7 +11,8 @@ screen.setup(600,600)
 cam_colors = ["white", "blue", "red", "black", "green"]
 
 def show_animatronics(cam_num):
-    if(cam_num == tina_path[])
+    if(cam_num == tina_path[tina_progress])
+    
 
 
 def open_cam_1():
